c0fd02fe0af77d2b3b6e3743ea5c6845b23a1dd2
akuutauf <taufikhidayatbwx09121@gmail.com>
2024-05-22 13:43:26 +0700

    Menambahkan Filter Get Mahasiswa By Angkatan dan Status Mahasiswa

--- src/controllers/mahasiswa.js
+++ src/controllers/mahasiswa.js
@@ -1,4 +1,4 @@
-const { Mahasiswa, Periode, Prodi } = require("../../models");
+const { Mahasiswa, Periode, Angkatan, StatusMahasiswa } = require("../../models");
 
 const getAllMahasiswa = async (req, res) => {
   try {
@@ -80,8 +80,103 @@ const getMahasiswaByProdiId = async (req, res, next) => {
   }
 };
 
+const getMahasiswaByAngkatanId = async (req, res, next) => {
+  try {
+    // Dapatkan ID angkatan dari parameter permintaan
+    const angkatanId = req.params.id_angkatan;
+
+    // Ambil data angkatan berdasarkan id_angkatan
+    const angkatan = await Angkatan.findOne({
+      where: {
+        id: angkatanId,
+      },
+    });
+
+    // Jika data angkatan tidak ditemukan, kirim respons 404
+    if (!angkatan) {
+      return res.status(404).json({
+        message: `Angkatan dengan ID ${angkatanId} tidak ditemukan`,
+      });
+    }
+
+    // Ekstrak tahun dari data angkatan
+    const tahunAngkatan = angkatan.tahun;
+
+    // Ambil semua data mahasiswa
+    const mahasiswas = await Mahasiswa.findAll();
+
+    // Filter data mahasiswa berdasarkan tahun angkatan yang dicocokkan dengan nama_periode_masuk
+    const filteredMahasiswas = mahasiswas.filter((mahasiswa) => {
+      const [tahunAwal] = mahasiswa.nama_periode_masuk.split("/");
+      return tahunAwal === tahunAngkatan;
+    });
+
+    // Jika data mahasiswa yang sesuai tidak ditemukan, kirim respons 404
+    if (filteredMahasiswas.length === 0) {
+      return res.status(404).json({
+        message: `Mahasiswa dengan tahun angkatan ${tahunAngkatan} tidak ditemukan`,
+      });
+    }
+
+    // Kirim respons JSON jika berhasil
+    res.status(200).json({
+      message: `GET Mahasiswa By Angkatan ID ${angkatanId} Success`,
+      jumlahData: filteredMahasiswas.length,
+      data: filteredMahasiswas,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMahasiswaByStatusMahasiswaId = async (req, res, next) => {
+  try {
+    // Dapatkan ID status mahasiswa dari parameter permintaan
+    const statusMahasiswaId = req.params.id_status_mahasiswa;
+
+    // Temukan status mahasiswa berdasarkan ID
+    const status_mahasiswa = await StatusMahasiswa.findOne({
+      where: {
+        id_status_mahasiswa: statusMahasiswaId,
+      },
+    });
+
+    // Jika status mahasiswa tidak ditemukan, kirim respons 404
+    if (!status_mahasiswa) {
+      return res.status(404).json({
+        message: `Status Mahasiswa dengan ID ${statusMahasiswaId} tidak ditemukan`,
+      });
+    }
+
+    // Cari data mahasiswa berdasarkan nama_status_mahasiswa yang sesuai
+    const mahasiswas = await Mahasiswa.findAll({
+      where: {
+        nama_status_mahasiswa: status_mahasiswa.nama_status_mahasiswa,
+      },
+    });
+
+    // Jika data mahasiswa tidak ditemukan, kirim respons 404
+    if (!mahasiswas || mahasiswas.length === 0) {
+      return res.status(404).json({
+        message: `Mahasiswa dengan status mahasiswa ${status_mahasiswa.nama_status_mahasiswa} tidak ditemukan`,
+      });
+    }
+
+    // Kirim respons JSON jika berhasil
+    res.status(200).json({
+      message: `GET Mahasiswa By Status Mahasiswa ${status_mahasiswa.nama_status_mahasiswa} Success`,
+      jumlahData: mahasiswas.length,
+      data: mahasiswas,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllMahasiswa,
   getMahasiswaById,
   getMahasiswaByProdiId,
+  getMahasiswaByAngkatanId,
+  getMahasiswaByStatusMahasiswaId,
 };
--- src/routes/mahasiswa.js
+++ src/routes/mahasiswa.js
@@ -10,5 +10,7 @@ const checkRole = require("../middlewares/check-role");
 router.get("/", checkRole(["admin", "admin-prodi"]), MahasiswaController.getAllMahasiswa);
 router.get("/:id/get", checkRole(["admin", "admin-prodi"]), MahasiswaController.getMahasiswaById);
 router.get("/prodi/:id_prodi/get", checkRole(["admin", "admin-prodi"]), MahasiswaController.getMahasiswaByProdiId);
+router.get("/angkatan/:id_angkatan/get", checkRole(["admin", "admin-prodi"]), MahasiswaController.getMahasiswaByAngkatanId);
+router.get("/status_mahasiswa/:id_status_mahasiswa/get", checkRole(["admin", "admin-prodi"]), MahasiswaController.getMahasiswaByStatusMahasiswaId);
 
 module.exports = router;
