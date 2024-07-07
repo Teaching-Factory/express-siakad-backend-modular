37140316ae87c2159f9c711a7cc58535545c552f
akuutauf <taufikhidayatbwx09121@gmail.com>
2024-07-07 20:36:28 +0700

    Menambahkan Fitur Import Nilai

--- src/routes/nilai-perkuliahan.js
+++ src/routes/nilai-perkuliahan.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
 
@@ -6,8 +7,12 @@ const router = express.Router();
 const NilaiPerkuliahanController = require("../controllers/nilai-perkuliahan");
 const checkRole = require("../middlewares/check-role");
 
+// Setup multer for file upload
+const upload = multer({ dest: "uploads/" });
+
 // all routes
 router.get("/:id_kelas_kuliah/get-peserta-kelas", checkRole(["admin", "admin-prodi", "dosen"]), NilaiPerkuliahanController.getPesertaKelasKuliahByKelasKuliahId);
 router.post("/:id_kelas_kuliah/penilaian-detail-perkuliahan-kelas", checkRole(["admin", "admin-prodi", "dosen"]), NilaiPerkuliahanController.createOrUpdatePenilaianByKelasKuliahId);
+router.post("/:id_kelas_kuliah/import-nilai-perkuliahan", checkRole(["admin", "admin-prodi", "dosen"]), upload.single("file"), NilaiPerkuliahanController.importNilaiPerkuliahan);
 
 module.exports = router;
