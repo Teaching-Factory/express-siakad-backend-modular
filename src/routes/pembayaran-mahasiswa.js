72c7ffb30d54ce682c570e156e10625c01562221
akuutauf <taufikhidayatbwx09121@gmail.com>
2024-07-12 21:20:58 +0700

    Menambahkan Endpoint API untuk fitur Pembayaran Mahasiswa

--- src/routes/pembayaran-mahasiswa.js
+++ src/routes/pembayaran-mahasiswa.js
@@ -22,10 +22,12 @@ const upload = multer({ storage: storage });
 
 // all routes
 router.get("/tagihan-mahasiswa/:id_tagihan_mahasiswa/get", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.getAllPembayaranMahasiswaByTagihanId);
+router.get("/get-pembayaran-dikonfirmasi", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.getAllPembayaranMahasiswaDikonfirmasi);
 router.get("/:id/get", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.getPembayaranMahasiswaById);
 router.get("/mahasiswa/:id_registrasi_mahasiswa/get", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.getPembayaranMahasiswaByMahasiswaId);
 router.post("/tagihan-mahasiswa/:id_tagihan_mahasiswa/create", checkRole(["admin", "admin-keuangan", "mahasiswa"]), upload.single("upload_bukti_tf"), PembayaranMahasiswaController.createPembayaranMahasiswaByTagihanId);
 router.put("/:id/update", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.updatePembayaranMahasiswaById);
+router.put("/:id/update-status-pembayaran", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.updateStatusPembayaranMahasiswaById);
 router.delete("/:id/delete", checkRole(["admin", "admin-keuangan", "mahasiswa"]), PembayaranMahasiswaController.deletePembayaranMahasiswaById);
 
 module.exports = router;
