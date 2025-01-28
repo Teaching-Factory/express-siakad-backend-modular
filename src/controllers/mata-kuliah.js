9034d54a0dfca371ec2940179990e71ca1742a51
akuutauf <taufikhidayatbwx09121@gmail.com>
2025-01-28 21:20:15 +0700

    Menambahkan endpoint matakuliah by prodi

--- src/controllers/mata-kuliah.js
+++ src/controllers/mata-kuliah.js
@@ -50,7 +50,46 @@ const getMataKuliahById = async (req, res, next) => {
   }
 };
 
+const getMataKuliahByProdiId = async (req, res, next) => {
+  try {
+    // Dapatkan ID dari parameter permintaan
+    const prodiId = req.params.id_prodi;
+
+    // Periksa apakah ID disediakan
+    if (!prodiId) {
+      return res.status(400).json({
+        message: "Prodi ID is required",
+      });
+    }
+
+    // Cari data mata_kuliahs berdasarkan ID di database
+    const mata_kuliahs = await MataKuliah.findAll({
+      where: {
+        id_prodi: prodiId,
+      },
+      include: [{ model: Prodi }],
+    });
+
+    // Jika data tidak ditemukan, kirim respons 404
+    if (!mata_kuliahs) {
+      return res.status(404).json({
+        message: `<===== Mata Kuliah By Prodi ID ${prodiId} Not Found:`,
+      });
+    }
+
+    // Kirim respons JSON jika berhasil
+    res.status(200).json({
+      message: `<===== GET Mata Kuliah By Prodi ID ${prodiId} Success:`,
+      jumlahData: mata_kuliahs.length,
+      data: mata_kuliahs,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllMataKuliah,
   getMataKuliahById,
+  getMataKuliahByProdiId,
 };
--- src/routes/mata-kuliah.js
+++ src/routes/mata-kuliah.js
@@ -9,5 +9,6 @@ const checkRole = require("../middlewares/check-role");
 // all routes
 router.get("/", checkRole(["admin", "admin-prodi", "dosen", "mahasiswa", "admin-keuangan"]), MataKuliahController.getAllMataKuliah);
 router.get("/:id/get", checkRole(["admin", "admin-prodi"]), MataKuliahController.getMataKuliahById);
+router.get("/prodi/:id_prodi/get", checkRole(["admin", "admin-prodi"]), MataKuliahController.getMataKuliahByProdiId);
 
 module.exports = router;
