dbe225f7dd2a022f8abc91f0be749ac18582858f
akuutauf <taufikhidayatbwx09121@gmail.com>
2025-02-07 10:18:48 +0700

    Menambahkan akses dosen ke komponen evaluasi kelas

--- src/routes/komponen-evaluasi-kelas.js
+++ src/routes/komponen-evaluasi-kelas.js
@@ -7,11 +7,11 @@ const KomponenEvaluasiKelasController = require("../controllers/komponen-evaluas
 const checkRole = require("../middlewares/check-role");
 
 // all routes
-router.get("/", checkRole(["admin", "admin-prodi"]), KomponenEvaluasiKelasController.getAllKomponenEvaluasiKelas);
-router.get("/:id/get", checkRole(["admin", "admin-prodi"]), KomponenEvaluasiKelasController.getKomponenEvaluasiKelasById);
-router.get("/kelas-kuliah/:id_kelas_kuliah/get", checkRole(["admin", "admin-prodi"]), KomponenEvaluasiKelasController.getKomponenEvaluasiKelasByKelasKuliahId);
-router.post("/kelas-kuliah/:id_kelas_kuliah/create", checkRole(["admin", "admin-prodi"]), KomponenEvaluasiKelasController.createOrUpdateKomponenEvaluasiKelas);
-router.post("/salin-komponen-evaluasi-kelas/:id_kelas_kuliah/create", checkRole(["admin", "admin-prodi"]), KomponenEvaluasiKelasController.copyKomponenEvaluasiKelasByMataKuliah);
-router.delete("/:id_komponen_evaluasi/delete", checkRole(["admin", "admin-prodi"]), KomponenEvaluasiKelasController.deleteKomponenEvaluasiKelasById);
+router.get("/", checkRole(["admin", "admin-prodi", "dosen"]), KomponenEvaluasiKelasController.getAllKomponenEvaluasiKelas);
+router.get("/:id/get", checkRole(["admin", "admin-prodi", "dosen"]), KomponenEvaluasiKelasController.getKomponenEvaluasiKelasById);
+router.get("/kelas-kuliah/:id_kelas_kuliah/get", checkRole(["admin", "admin-prodi", "dosen"]), KomponenEvaluasiKelasController.getKomponenEvaluasiKelasByKelasKuliahId);
+router.post("/kelas-kuliah/:id_kelas_kuliah/create", checkRole(["admin", "admin-prodi", "dosen"]), KomponenEvaluasiKelasController.createOrUpdateKomponenEvaluasiKelas);
+router.post("/salin-komponen-evaluasi-kelas/:id_kelas_kuliah/create", checkRole(["admin", "admin-prodi", "dosen"]), KomponenEvaluasiKelasController.copyKomponenEvaluasiKelasByMataKuliah);
+router.delete("/:id_komponen_evaluasi/delete", checkRole(["admin", "admin-prodi", "dosen"]), KomponenEvaluasiKelasController.deleteKomponenEvaluasiKelasById);
 
 module.exports = router;
